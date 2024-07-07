50ba6848a601e91335ad4d204b80d0518fe6e6ac
yoshikawa masashi <gunterriote@yahoo.co.jp>
2024-07-08 00:50:11 +0900

    resolve 2-4

--- src/main/java/chapter2/practice4.java
+++ src/main/java/chapter2/practice4.java
@@ -6,7 +6,7 @@ import java.util.stream.Stream;
 public class practice4 {
     public static void main(String[] args) {
         int[] values = {1, 4, 9, 16};
-        Stream.of(values).forEach(System.out::println); // 配列の個々の要素ではなくint型配列が出力される
+        Stream.of(values).forEach(arr -> System.out.println(Arrays.toString(arr))); // 配列の個々の要素ではなくint型配列が出力される
         Arrays.stream(values).forEach(System.out::println); // 配列の個々の要素が出力される
     }
 }
